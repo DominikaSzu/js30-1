0cb187f2dd12c59aab04d9c74a77290067c67ddf
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-11 12:53:21 +0200

    Audio plays when key button pressed

--- js/app.js
+++ js/app.js
@@ -1,38 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(e) {
-    
-//    keybord buttons
-    
-    const keyA = document.getElementById('65');
-    const keyS = document.getElementById('83');
-    const keyD = document.getElementById('68');
-    const keyF = document.getElementById('70');
-    const keyG = document.getElementById('71');
-    const keyH = document.getElementById('72');
-    const keyJ = document.getElementById('74');
-    const keyK = document.getElementById('75');
-    const keyL = document.getElementById('76');
-    
+
     checkWhichKeyPressed = (event) => {
-//        if (event.keyCode == '65') {
-//            console.log(keyA);
-//        } else if (event.keyCode == '83') {
-//            console.log(keyS);
-//        } else if (event.keyCode == '68') {
-//            console.log(keyD);
-//        } else if (event.keyCode == '70') {
-//            console.log(keyF);
-//        } else if (event.keyCode == '71') {
-//            console.log(keyG);
-//        } else if (event.keyCode == '72') {
-//            console.log(keyH);
-//        } else if (event.keyCode == '74') {
-//            console.log(keyJ);
-//        } else if (event.keyCode == '75') {
-//            console.log(keyK);
-//        } else if (event.keyCode == '76') {
-//            console.log(keyL);
-//        }
-        console.log(event.keyCode)
+
+        let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+        
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        let button = document.querySelector(`div[data-key="${event.keyCode}"]`);
+        console.log(button)
+        button.classList.add('playing');
     }
     
     
